66e5341525b2829baededc381d84b60a147f6450
gdilthey <george.dilthey@gmail.com>
2021-01-17 17:09:29 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuz(num)
-
+  if num % 15 == 0
+    "FizzBuzz"
+  else num % 3 == 0
+    "Fizz"
+  else num % 5 == 0
+    "Buzz"
+  end
 end
